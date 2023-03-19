1a885a4256c9a8d26b1fd77fd0515fea8e8f5512
Daniele Costarella <daniele.costarella@gmail.com>
2023-03-19 14:50:48 +0100

    Add a template to create the config.py file

---
+++ config-template.py
@@ -0,0 +1,20 @@
+# This is a template. Rename to creds.py.
+# Please fill with your secret data.
+# Remember to not commit this file in your projects
+
+## Serial Port configuration
+serial_port='/dev/cu.usbmodem11101' # it could be '/dev/ttyUSB0' or similar
+baudrate = 9600
+timeout = 1
+
+## Telegram support
+TELEGRAM_NOTIFY=False
+
+# Telegram
+api_id = <YOUR_API>
+api_hash = 'API_HASH'
+bot_token = 'YOUR_TELEGRAM_BOT_TOKEN'
+phone = 'YOUR_MOBILE_NUMBER_USED_FOR_TELEGRAM'
+chat_id='YOUR_CHAT_ID'
+recipient='USER_NAME_USED_AS_RECIPIENT'
+api_session='API_SESSION'
